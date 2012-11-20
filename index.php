156d831858cc33c6563f89329c61ff667a23259d
hitomi sho <hitomi@to8.co.jp>
2012-11-20 16:18:06 +0900

    test
    github test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>git test</title>
+    </head>
+    <body>
+        <?php
+        //git test
+        ?>
+    </body>
+</html>
